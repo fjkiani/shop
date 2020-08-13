1c0479ab07294795bba68c4d5d088c7a84642b93
fahad <fjkiani@icloud.com>
2020-08-12 21:35:59 -0400

    init

---
+++ database/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd3ca32cddd9abb1433589baded5acf19'),
+    },
+  },
+});
---
+++ database/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1253d95b-0721-402e-9069-e03b53f865b8'
+};
